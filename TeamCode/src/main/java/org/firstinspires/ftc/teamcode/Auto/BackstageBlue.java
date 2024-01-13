91a032dcb257281f19197aa574b2202b4423aa50
CowCowFuture <eryckdg@hotmail.com>
2024-01-12 18:46:42 -0600

    Fix warnings
    a long way away from "-Werror -Wall -Wextra -pedantic" but getting there

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BackstageBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BackstageBlue.java
@@ -8,18 +8,15 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Backstage Blue", group="test")
 public class BackstageBlue extends LinearOpMode {
-    private DcMotor leftDrive;
-    private DcMotor rightDrive;
-	private Servo fingerServo;
 
-    private ElapsedTime runtime = new ElapsedTime();
+	private final ElapsedTime runtime = new ElapsedTime();
 
 
     @Override
     public void runOpMode() {
-		leftDrive  = hardwareMap.get(DcMotor.class, "leftMotor");
-		rightDrive = hardwareMap.get(DcMotor.class, "rightMotor");
-		fingerServo = hardwareMap.get(Servo.class, "finger_servo");
+		DcMotor leftDrive = hardwareMap.get(DcMotor.class, "leftMotor");
+		DcMotor rightDrive = hardwareMap.get(DcMotor.class, "rightMotor");
+		Servo fingerServo = hardwareMap.get(Servo.class, "finger_servo");
 		fingerServo.setPosition(0.4);
 
 		waitForStart();
@@ -27,13 +24,13 @@ public class BackstageBlue extends LinearOpMode {
 		rightDrive.setDirection(DcMotor.Direction.FORWARD);
 
 		MoveByEncoder.encoderDrive(.25, 32, 5,
-					   leftDrive, rightDrive);
+				leftDrive, rightDrive);
 	    MoveByEncoder.encoderDrive(.25, -6, 5,
-	    	    leftDrive, rightDrive);
+				leftDrive, rightDrive);
 	    MoveByEncoder.encoderTurn(0.25, -90, 5,
-			    leftDrive, rightDrive);
+				leftDrive, rightDrive);
 	    MoveByEncoder.encoderDrive(.25, 38, 5,
-			    leftDrive, rightDrive);
+				leftDrive, rightDrive);
 		MoveByEncoder.encoderDrive(.25, -5, 5,
 				leftDrive, rightDrive);
 		fingerServo.setPosition(0.2);
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BackstageRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Auto/BackstageRed.java
@@ -9,18 +9,15 @@ import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="Backstage Red", group="test")
 public class BackstageRed extends LinearOpMode {
-	private DcMotor leftDrive;
-	private DcMotor rightDrive;
-	private Servo fingerServo;
 
-    private ElapsedTime runtime = new ElapsedTime();
+	private final ElapsedTime runtime = new ElapsedTime();
 
 
     @Override
     public void runOpMode() {
-		leftDrive  = hardwareMap.get(DcMotor.class, "leftMotor");
-		rightDrive = hardwareMap.get(DcMotor.class, "rightMotor");
-		fingerServo = hardwareMap.get(Servo.class, "finger_servo");
+		DcMotor leftDrive = hardwareMap.get(DcMotor.class, "leftMotor");
+		DcMotor rightDrive = hardwareMap.get(DcMotor.class, "rightMotor");
+		Servo fingerServo = hardwareMap.get(Servo.class, "finger_servo");
 		fingerServo.setPosition(0.4);
 
 		waitForStart();
@@ -29,14 +26,14 @@ public class BackstageRed extends LinearOpMode {
 
 		// Place first pixel on the center & turn.
 		MoveByEncoder.encoderDrive(.25, 32, 5,
-					   leftDrive, rightDrive);
+				leftDrive, rightDrive);
 	    MoveByEncoder.encoderDrive(.25, -6, 5,
-	    	    leftDrive, rightDrive);
+				leftDrive, rightDrive);
 	    MoveByEncoder.encoderTurn(0.25, 90, 5,
-			    leftDrive, rightDrive);
+				leftDrive, rightDrive);
 		// Score second pixel on board.
 	    MoveByEncoder.encoderDrive(.25, 38, 5,
-			    leftDrive, rightDrive);
+				leftDrive, rightDrive);
 		fingerServo.setPosition(0.2);
     }
 }
